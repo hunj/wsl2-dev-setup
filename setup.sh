25cb5163536ceb9f987dda566a5d3c3a776ff9be
Hun Jae Lee <hunjaelee@protonmail.com>
2022-07-07 13:48:56 -0400

    base works

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install zsh
+brew install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
